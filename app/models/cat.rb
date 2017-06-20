53eb1b727abb6b02e270bf1cc1d3e0f29cf72183
Spencer Chan <schan1031@gmail.com>
2017-06-20 11:50:24 -0700

    99Cats rails setup, cats index page

---
+++ app/models/cat.rb
@@ -0,0 +1,10 @@
+class Cat < ApplicationRecord
+  VALID_COLORS = %w(Black White Brown Grey Orange Silver Blue Red Green).freeze
+  validates :birth_date, :name, presence: true
+  validates :sex, inclusion: { in: %w(M F), message: 'Invalid Sex' }
+  validates :color, inclusion: { in: VALID_COLORS, message: 'Invalid Color' }
+
+  def age
+    Time.now.year - self.birth_date.year
+  end
+end
